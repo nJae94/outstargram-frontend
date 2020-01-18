343d808e11e4e3279082b8ce3765dde289a6fa57
sunjae <azuretic94@gmail.com>
2020-01-18 23:41:20 +0900

    검색 추가 중

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,20 +2,56 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
+import Loader from "../../Components/Loader";
+import UserCard from "../../Components/UserCard";
 
 const Wrapper = styled.div`
   height: 50vh;
-  text-align: center;
 `;
 
-const SearchPresenter = ({ searchTerm, loading, data }) => (
+const Section = styled.div``;
 
-  <Wrapper>
-    {searchTerm === undefined && <FatText text={"다시 검색해주세요..."} />}
-
-    {searchTerm !== undefined && <FatText text={"검색"} />}
-  </Wrapper>
-);
+const SearchPresenter = ({ searchTerm, loading, data }) => {
+  if (searchTerm === undefined) {
+    return (
+      <Wrapper>
+        <FatText text="검색..." />
+      </Wrapper>
+    );
+  } else if (loading === true) {
+    return (
+      <Wrapper>
+        <Loader />
+      </Wrapper>
+    );
+  } else if (data && data.searchUser && data.searchPost) {
+    return (
+      <Wrapper>
+        <Section>
+          {data.searchUser.length === 0 ? (
+            <FatText text="유저를 찾을 수 없습니다." />
+          ) : (
+            data.searchUser.map(user => (
+              <UserCard
+                username={user.username}
+                isFollowing={user.isFollowing}
+                url={user.url}
+                isSelf={user.isSelf}
+              />
+            ))
+          )}
+        </Section>
+        <Section>
+          {data.searchPost.length === 0 ? (
+            <FatText text="포스트를 찾을 수 없습니다." />
+          ) : (
+            data.searchPost.map(post => null)
+          )}
+        </Section>
+      </Wrapper>
+    );
+  }
+};
 
 SearchPresenter.propTypes = {
   searchTerm: PropTypes.string,
